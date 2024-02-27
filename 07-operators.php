312e751e267aa00f4f1431060cd573d87c73ffc7
GilaniRabbu <toufiqjilanirabbu7@gmail.com>
2024-02-28 00:02:56 +0600

    php operators

---
+++ 07-operators.php
@@ -0,0 +1,33 @@
+<?php
+// $number = 12 + 3; // operand operator
+
+$number = 12;
+// $number = $number + 13;
+// $number += 13;
+
+$number2 = $number - 20; // -8
+
+$number = 12 * 2; // 24
+$number = 12 / 2; // 6
+$number = 12 % 2; // 0
+
+$number3 = (13 - 5) * 6; // 48
+$number3 = (24 - 4) / (7 - 2); // 4
+
+echo $number;
+echo $number2;
+echo $number3;
+
+$num = 15;
+$num++; // $num += 1; $num = $num + 1; // Post-increment
+// ++$num; // Output - 16 // Pre-increment
+echo $num;
+
+// Assign multiple variables in one line
+$num1 = $num2 = 5;
+echo $num1, "\n", $num2;
+
+$txt1 = "Hello";
+$txt2 = " World!";
+echo $txt1 . $txt2; // Hello World!
+?>
