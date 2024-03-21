aef8f3302c6bcd2074069fcec1211e3bdaa5d61e
GilaniRabbu <toufiqjilanirabbu7@gmail.com>
2024-03-22 03:55:20 +0600

    String

--- 05-string.php
+++ 05-string.php
@@ -1,53 +1,38 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+$name = "Tr Gilani";
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP String</title>
-</head>
+# Length
+echo strlen($name) . "\n";
 
-<body>
-    <h1>PHP String</h1>
-    <p>A string is a sequence of characters, like "Hello world!".</p>
-    <p>Strings in PHP are surrounded by either double quotation marks, or single quotation marks.</p>
+# Word Count
+echo str_word_count($name) . "\n";
 
-    <?php
-    $name = "Tr Gilani";
+# Search For Text Within a String
+echo strpos($name, "Gilani") . "\n";
 
-    # Length
-    echo strlen($name) . "<br>";
+# Upper Case
+echo strtoupper($name) . "\n";
 
-    # Word Count
-    echo str_word_count($name) . "<br>";
+# Lower Case
+echo strtolower($name) . "\n";
 
-    # Upper Case
-    echo strtoupper($name) . "<br>";
+# Reverse a String
+echo strrev($name) . "\n";
 
-    # Lower Case
-    echo strtolower($name) . "<br>";
+# Replace String
+echo str_replace("Tr", "MD", $name) . "\n";
 
-    # Reverse a String
-    echo strrev($name) . "<br>";
+# Convert String into Array
+$place = "Eastern United Front";
+$con = explode(" ", $place);
+print_r($con);
 
-    # Replace String
-    echo str_replace("Tr", "MD", $name) . "<br>";
+# Slicing Strings
+$txt = "Hello World";
+echo substr($txt, 6, 5) . "\n"; # World
+echo substr($txt, -5, 3) . "\n"; # Wor
 
-    # Convert String into Array
-    $place = "Eastern United Front";
-    $con = explode(" ", $place);
-    print_r($con);
-
-    # Slicing Strings
-    $txt = "Hello World";
-    echo substr($txt, 6, 5) . "<br>"; # World
-    echo substr($txt, -5, 3) . "<br>"; # Wor
-    
-    # Escape Character
-    $newTxt = "We are the so-called \"Vikings\" from the north.";
-    echo $newTxt;
-    ?>
-</body>
-
-</html>
+# Escape Character
+$newTxt = "We are the so-called \"Vikings\" from the north.";
+echo $newTxt;
+?>
